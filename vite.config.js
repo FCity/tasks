704c93cd42048c7192d2ae35ff9b930f8fc71cc9
Francisco Calderon <thefcalderon@gmail.com>
2023-08-30 15:04:18 -0700

    ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/FCity/tasks',
   plugins: [react()],
+  base: '/tasks',
   server: {
     port: 3000,
     proxy: {
